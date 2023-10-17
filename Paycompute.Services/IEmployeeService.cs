ce57dc9856d6f813b2407e13d57788ba6180db9d
Dominik Štefičar <dominik.steficar@gmail.com>
2023-10-17 20:06:46 +0200

    Service implementation
    Added service implementation for the employee  db context.

---
+++ Paycompute.Services/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using Paycompute.Entity;
+
+namespace Paycompute.Services
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee newEmployee);
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task Delete(int employeeId);
+        decimal UnionFees(int id);
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount);
+        IEnumerable<Employee> GetAll();
+    }
+}
